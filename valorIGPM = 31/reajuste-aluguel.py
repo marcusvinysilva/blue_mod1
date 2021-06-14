be79b6c02e00a54e2eb1cc74429343b7c86cf61a
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2021-06-14 20:24:45 -0300

    Delete valorIGPM = 31 directory

--- valorIGPM = 31/reajuste-aluguel.py
+++
@@ -1,5 +0,0 @@
-valorIGPM = 31 / 100
-
-valorAluguel = float(input('Qual o valor do seu aluguel?'))
-
-print(f'O seu aluguel reajustado ficará no valor de {(valorAluguel + (valorAluguel * valorIGPM)):.2f} reais.')
