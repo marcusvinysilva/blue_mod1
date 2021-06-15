743ee2573d68a7cecf8765764246ebfea7514973
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2021-06-14 22:00:14 -0300

    Update ajuste-aluguel.py

--- ajuste-aluguel.py
+++ ajuste-aluguel.py
@@ -1,3 +1,7 @@
+# Calculadora de aumento de aluguel
+# Vamos construir um programa que irá calcular o aumento anual do seu aluguel.
+# A sua calculadora vai receber o valor do aluguel e calcular o aumento baseado no IGPM de 31%. A calculadora deve apresentar o aluguel reajustado no formato R$ XXXX.XX.
+
 valorIGPM = 31 / 100
 
 valorAluguel = float(input('Qual o valor do seu aluguel?'))
